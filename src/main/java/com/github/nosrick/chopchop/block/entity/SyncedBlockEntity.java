05105775667f81834ae23913458cb9420f5e7af4
nosrick <nosrick@gmail.com>
2022-08-24 14:47:17 +0100

    Adds SyncedBlockEntity for BlockEntity inheritance

---
+++ src/main/java/com/github/nosrick/chopchop/block/entity/SyncedBlockEntity.java
@@ -0,0 +1,37 @@
+package com.github.nosrick.chopchop.block.entity;
+
+import com.github.nosrick.chopchop.util.BlockStateUtils;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.Packet;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+public class SyncedBlockEntity extends BlockEntity {
+
+    public SyncedBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientPlayPacketListener> toUpdatePacket() {
+        return BlockEntityUpdateS2CPacket.create(this);
+    }
+
+    @Override
+    public NbtCompound toInitialChunkDataNbt() {
+        return createNbt();
+    }
+
+    public void inventoryChanged() {
+        this.markDirty();
+        if (this.world != null) {
+           this.world.updateListeners(this.getPos(), this.getCachedState(), this.getCachedState(), BlockStateUtils.DEFAULT);
+        }
+    }
+}
