e498c2807c67e8dfda230f22c1e849e3fe4ac61a
nosrick <nosrick@gmail.com>
2022-09-02 16:23:02 +0100

    Beginning of data gen stuff

---
+++ src/main/java/com/github/nosrick/chopchop/datagen/ChopChopDataGen.java
@@ -0,0 +1,11 @@
+package com.github.nosrick.chopchop.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChopChopDataGen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
