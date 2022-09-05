2328acabbc376fdf487866e420d183cfe170980f
nosrick <nosrick@gmail.com>
2022-09-05 16:17:38 +0100

    Finally, the chopping board renders!

--- src/main/java/com/github/nosrick/chopchop/block/ChoppingBoardBlock.java
+++ src/main/java/com/github/nosrick/chopchop/block/ChoppingBoardBlock.java
@@ -1,11 +1,12 @@
 package com.github.nosrick.chopchop.block;
 
 import com.github.nosrick.chopchop.ChopChopMod;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import com.github.nosrick.chopchop.block.entity.BlockEntityTypeRegistry;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,12 +15,18 @@ public class ChoppingBoardBlock extends BlockWithEntity {
     public ChoppingBoardBlock() {
         super(ChopChopMod.DefaultBlockSettings
                 .strength(0.5f)
-                .nonOpaque());
+                .nonOpaque()
+                .sounds(BlockSoundGroup.WOOD));
     }
 
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return null;
+        return BlockEntityTypeRegistry.CHOPPING_BOARD.get().instantiate(pos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
     }
 }
