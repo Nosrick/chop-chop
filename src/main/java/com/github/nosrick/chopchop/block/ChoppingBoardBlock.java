db8785773bd452426932cae4d7d3fd263f1af97c
nosrick <nosrick@gmail.com>
2022-09-21 15:36:35 +0100

    Chopping board inventory handles items correctly now

--- src/main/java/com/github/nosrick/chopchop/block/ChoppingBoardBlock.java
+++ src/main/java/com/github/nosrick/chopchop/block/ChoppingBoardBlock.java
@@ -2,15 +2,21 @@ package com.github.nosrick.chopchop.block;
 
 import com.github.nosrick.chopchop.ChopChopMod;
 import com.github.nosrick.chopchop.block.entity.BlockEntityTypeRegistry;
+import com.github.nosrick.chopchop.block.entity.ChoppingBoardBlockEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -44,6 +50,33 @@ public class ChoppingBoardBlock extends BlockWithEntity {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return BlockEntityTypeRegistry.CHOPPING_BOARD.get().instantiate(pos, state);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+        if(world.isClient || hand == Hand.OFF_HAND) {
+            return ActionResult.PASS;
+        }
+
+        if(!(world.getBlockEntity(pos) instanceof ChoppingBoardBlockEntity blockEntity)) {
+            return ActionResult.FAIL;
+        }
+
+        ItemStack itemStack = player.getMainHandStack();
+        if(itemStack.isFood()){
+            blockEntity.addItemStack(itemStack);
+
+            return ActionResult.SUCCESS;
+        }
+        else if(itemStack.isEmpty() && !blockEntity.isEmpty()) {
+            ItemStack fromChoppingBoard = blockEntity.removeItemStack();
+            player.giveItemStack(fromChoppingBoard);
+
+            return ActionResult.SUCCESS;
+        }
+
+        return ActionResult.CONSUME;
     }
 
     @Override
--- src/main/java/com/github/nosrick/chopchop/block/entity/ChoppingBoardBlockEntity.java
+++ src/main/java/com/github/nosrick/chopchop/block/entity/ChoppingBoardBlockEntity.java
@@ -2,6 +2,7 @@ package com.github.nosrick.chopchop.block.entity;
 
 import com.github.nosrick.chopchop.util.SingleStackInventory;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 
@@ -13,6 +14,29 @@ public class ChoppingBoardBlockEntity extends SyncedBlockEntity {
         super(BlockEntityTypeRegistry.CHOPPING_BOARD.get(), pos, state);
         this.inventory = new SingleStackInventory();
     }
+
+    public boolean addItemStack(ItemStack itemStack) {
+        if(this.isEmpty() && !itemStack.isEmpty()) {
+            this.inventory.setStack(0, itemStack.split(1));
+            this.inventoryChanged();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public ItemStack removeItemStack() {
+        if(!this.isEmpty()) {
+            ItemStack itemStack = this.inventory.removeStack(0);
+            this.inventoryChanged();
+            return itemStack;
+        }
+
+        return ItemStack.EMPTY;
+    }
+
+    public boolean isEmpty() { return this.inventory.getStack(0).isEmpty(); }
 
     @Override
     public void readNbt(NbtCompound nbt) {
